fcf28333a7c2bc4d70522f6796bb99daf8c0827a
liqiao <liqiao@onemt.com.cn>
2018-01-02 12:28:56 +0800

    M#crawler#参数处理优化

--- src/main/java/com/onemt/agent/log/LogOutput.java
+++ src/main/java/com/onemt/agent/log/LogOutput.java
@@ -7,9 +7,12 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.onemt.agent.annotation.TraceMethod;
 
 import zipkin2.Span;
 import zipkin2.Span.Builder;
@@ -23,32 +26,33 @@ public class LogOutput {
 	
 	private static final Gson gson=new GsonBuilder().create();
 	private static final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(100);
+	private static final String regex ="(\")?url(\")?\\s*[:=]\\s*[\"']([^\"']*)[\"']";
+	private static final Pattern compile = Pattern.compile(regex);
 	
 	
 	public static void spanOutput(final Builder builder,final Method method,final Object[] arguments,final Object retVal,final Throwable throwable) {
 		newFixedThreadPool.execute(()->{
 			//AsyncReporter<Span> reporter = AsyncReporter.create(URLConnectionSender.create(apiUrl));
 			//KafkaSender kafkaSender = KafkaSender.newBuilder().bootstrapServers(bootstrapServers).topic("zipkin").encoding(Encoding.JSON).build();
-			//List<String> argumentList = new ArrayList<>();
+			TraceMethod annotation = method.getAnnotation(TraceMethod.class);
+			boolean isStart = annotation.isStart();
 			if(arguments!=null){
-				/*for (Object object : arguments) {
-					if(object != null){
-						if(object instanceof Serializable){
-							argumentList.add(gson.toJson(object));
-						}else{
-							argumentList.add(object.toString());
-						}
-					}
-				}
-				builder.putTag("arguments", gson.toJson(argumentList));*/
 				Parameter[] parameters = method.getParameters();
 				for (int i = 0; i < parameters.length; i++) {
 					String parameterName = parameters[i].getName();
 					Object parameterValue =arguments[i];
 					if(parameterValue instanceof Serializable){
-						builder.putTag("parameterName:"+parameterName, gson.toJson(parameterValue));
+						 String parameterVal = gson.toJson(parameterValue);
+						builder.putTag("parameterName:"+parameterName, parameterVal);
+						if(isStart){
+							Matcher matcher = compile.matcher(parameterVal);
+							while(matcher.find()){
+								builder.putTag("url", matcher.group(3));
+								break;
+							}
+						}
 					}else{
-						builder.putTag("parameterName:"+parameterName, parameterValue.toString());
+						builder.putTag("parameterName:"+parameterName, parameterValue!=null?parameterValue.toString():null);
 					}
 				}
 			}
@@ -72,6 +76,16 @@ public class LogOutput {
 		StringWriter sw = new StringWriter();
 		t.printStackTrace(new PrintWriter(sw, true));
 		return sw.getBuffer().toString();
+	}
+	
+	public static void main(String[] args) {
+		String regex ="(\")?url(\")?\\s*[:=]\\s*[\"']([^\"']*)[\"']";
+		String parameterVal="equest=Request{url='http://www.alriyadh.com/1564426', method='null'";
+		Pattern compile = Pattern.compile(regex);
+		Matcher matcher = compile.matcher(parameterVal);
+		while(matcher.find()){
+			System.out.println(matcher.group(3));
+		}
 	}
 
 }
